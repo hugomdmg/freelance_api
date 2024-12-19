e8b8b680d94ce7e5a908ae42aaf3c0fbc9ba9b8c
hugomdmg <hugomdmg@gmail.com>
2024-12-19 23:04:57 +0000

    refactor

--- src/routes/chats.js
+++ src/routes/chats.js
@@ -8,14 +8,14 @@ router.post('/get-messages', async (req, res) => {
     const data = req.body;
 
     try {
-        const user = await db.getFilteredItems('users', { email: data.user1.email });
+        const user = await db.getFilteredItems('users', { email: data.user1 });
 
         if (user.length === 0) {
             return res.status(404).send({ status: 404, data: 'User not found' });
         }
 
         for (const chat of user[0].chats) {
-            if (chat.user === data.user2.email) {
+            if (chat.user === data.user2) {
                 return res.send({ status: 200, data: chat });
             }
         }
@@ -33,53 +33,48 @@ router.post('/send-message', async (req, res) => {
     const data = req.body;
 
     try {
-        // Buscar usuarios por email
-        const user1 = await db.getFilteredItems('users', { email: data.user1.email });
-        const user2 = await db.getFilteredItems('users', { email: data.user2.email });
+        const [user1] = await db.getFilteredItems('users', { email: data.emailUser1 });
+        const [user2] = await db.getFilteredItems('users', { email: data.emailUser2 });
 
-        if (!user1.length || !user2.length) {
+        if (!user1 || !user2) {
             return res.status(404).send({ status: 404, value: 'Users not found' });
         }
 
-        // Referencias a los objetos principales
-        const user1Data = user1[0];
-        const user2Data = user2[0];
-
-        // Actualizar chats para user1
         let chatUpdated1 = false;
-        user1Data.chats.forEach((chat, index) => {
-            if (chat.user === data.user2.email) {
-                user1Data.chats[index].messages = data.messages;
+        let index1 = 0
+        user1.chats.forEach((chat, i) => {
+            if (chat.user === data.emailUser2) {
+                user1.chats[i].messages.push({ owner: data.emailUser1, message: data.message });
+                index1 = i
                 chatUpdated1 = true;
             }
         });
         if (!chatUpdated1) {
-            user1Data.chats.push({
-                user: data.user2.email,
-                messages: data.messages,
+            user1.chats[index1].messages.push({
+                user: data.emailUser2,
+                messages: [data.message],
             });
         }
 
-        // Actualizar chats para user2
         let chatUpdated2 = false;
-        user2Data.chats.forEach((chat, index) => {
-            if (chat.user === data.user1.email) {
-                user2Data.chats[index].messages = data.messages;
+        let index2 = 0
+        user2.chats.forEach((chat, i) => {
+            if (chat.user === data.emailUser1) {
+                user2.chats[i].messages.push({ owner: data.emailUser1, message: data.message })
+                index2 = i
                 chatUpdated2 = true;
             }
         });
         if (!chatUpdated2) {
-            user2Data.chats.push({
-                user: data.user1.email,
-                messages: data.messages,
+            user2.chats[index2].messages.push({
+                user: data.emailUser1,
+                messages: [data.message],
             });
         }
 
-        // Guardar usuarios actualizados en la base de datos
-        await db.updateItem('users', { email: data.user1.email }, user1Data);
-        await db.updateItem('users', { email: data.user2.email }, user2Data);
+        await db.updateItem('users', { email: data.emailUser1 }, user1);
+        await db.updateItem('users', { email: data.emailUser2 }, user2);
 
-        // Respuesta exitosa
         res.status(200).send({ status: 200, value: 'Message sent successfully' });
 
     } catch (error) {
